6c60f57b113e8673bff8ae250da40a1457ba8e92
nitin <nitinguptamca@gmail.com>
2018-12-18 00:15:22 +0530

    update

---
+++ src/company/Trade.java
@@ -0,0 +1,43 @@
+package company;
+
+
+public class Trade {
+
+	public enum Type {
+		BUY, SELL
+	}
+
+	private Type type;
+
+	private int quantity;
+
+	private double price;
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getValue() {
+		return quantity * price;
+	}
+}
---
+++ src/company/synechron/CompletableFutureTest.java
@@ -0,0 +1,76 @@
+package company.synechron;
+
+import java.net.PasswordAuthentication;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * What is completableFuture Explain test case when to use
+ * https://www.baeldung.com/java-completablefuture
+ * https://www.callicoder.com/java-8-completablefuture-tutorial/
+ * https://dzone.com/articles/20-examples-of-using-javas-completablefuture
+ * http://www.deadcoderising.com/java8-writing-asynchronous-code-with-completablefuture/
+ * https://www.youtube.com/watch?v=ImtZgX1nmr8
+ * 
+ * @author nitin
+ *
+ */
+
+public class CompletableFutureTest {
+	final static Logger LOGGER = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+	public static <T> CompletableFuture<T> repeat(final Supplier<T> action, final Predicate<T> until) {
+
+		return CompletableFuture.supplyAsync(() -> Stream.generate(action).filter(until).findFirst().get());
+	}
+
+	private static <T> T getSuppliedValue(final CompletableFuture<T> futureAction) {
+		try {
+			return futureAction.get();
+		} catch (InterruptedException | ExecutionException e) {
+			LOGGER.error(e.getMessage());
+		}
+
+		return null;
+	}
+
+	public void testRepeat() {
+		Supplier<PasswordAuthentication> action = () -> {
+			final String[] passwordVault = new String[] { "password", "secret", "secretPassword" };
+
+			final int len = passwordVault.length;
+
+			return new PasswordAuthentication("mickeyMouse",
+					passwordVault[ThreadLocalRandom.current().nextInt(len)].toCharArray());
+		};
+		@SuppressWarnings("static-access")
+		Predicate<PasswordAuthentication> until = passwordAuth -> {
+			try {
+				Thread.currentThread().sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+
+			final String password = String.valueOf(passwordAuth.getPassword());
+
+			LOGGER.info("Received password: {}.", password);
+
+			return password.equals("secret");
+		};
+
+		repeat(action, until);
+	}
+
+	public static void main(String[] args) {
+		CompletableFutureTest completableFutureTest = new CompletableFutureTest();
+		completableFutureTest.testRepeat();
+	}
+
+}
